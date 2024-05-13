b8a6c7cb0a7f5b0c1a6de3ddcaa07698a0dfbc9e
nameoz <avikpl44@gmail.com>
2024-05-13 15:16:15 +0530

    add garnet

---
+++ src/main/java/net/avik/mccourse/item/ModItems.java
@@ -0,0 +1,32 @@
+package net.avik.mccourse.item;
+
+import net.avik.mccourse.MCCourse;
+import net.fabricmc.fabric.api.item.v1.FabricItem;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item PINK_GARNET = registerItem("pink_garnet",
+            new Item(new FabricItemSettings()));
+    public static final  Item RAW_PINK_GARNET = registerItem("raw_pink_garnet",
+            new Item(new FabricItemSettings()));
+    public static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM,new Identifier(MCCourse.MOD_ID,name),item);
+    }
+    public static void itemGroupIngredients(FabricItemGroupEntries entries){
+        entries.add(PINK_GARNET);
+        entries.add(RAW_PINK_GARNET);
+    }
+    public static void registerModItems(){
+        MCCourse.LOGGER.info("Registering Mod Items for" + MCCourse.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::itemGroupIngredients);
+    }
+}
